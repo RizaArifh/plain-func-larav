1f6566f38adf0aca84ef13fbcc18b7789ae3831a
rizaarifh <rizaarifh338@gmail.com>
2020-11-30 14:19:18 +0700

    login ui

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login',[LoginController::class,'index'])->name('login.index');
+Auth::routes();
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
