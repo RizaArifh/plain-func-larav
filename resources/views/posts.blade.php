5714e3dcfce4df46477216c199a822904fd72feb
RizaArifh <rizaarifh338@gmail.com>
2020-12-07 11:23:00 +0700

    crud post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,15 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Posts</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
+
 <body>
-    <h1>all post</h1>
-    @foreach($posts as $post)
-    <h3>{{$post->title}}</h3>
-    <h3>{{$post->body}}</h3>
-    @endforeach
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            All Post
+                        </div>
+                        <div class="card-body">
+                        @if(Session::has('post_deleted'))
+                            <div class="alert alert-danger" role="alert">
+                                {{Session::get('post_deleted')}}
+                            </div>
+                            @endif
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Post Title</th>
+                                        <th>Post Body</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        @foreach ($posts as $post)
+                                        <tr>
+                                            <td>{{$post->title}}</td>
+                                            <td>{{$post->body}}</td>
+                                            <td>
+                                                <a class="btn btn-success" href="/posts/{{$post->id}}">View</a>
+                                                <a class="btn btn-info" href="/edit-post/{{$post->id}}">Edit</a>
+                                                <a class="btn btn-danger" href="/delete-post/{{$post->id}}">Delete</a>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 </body>
+
 </html>
