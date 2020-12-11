ef17283a92fec5521f06460e8a53f735deada1bf
RizaArifh <rizaarifh338@gmail.com>
2020-12-11 15:31:31 +0700

    testing toast,sweetalert add stud

--- resources/views/add-student.blade.php
+++ resources/views/add-student.blade.php
@@ -2,6 +2,7 @@
 @extends('layouts.navb')
 @section('title','Add Student')
 @section('headcontent')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA==" crossorigin="anonymous" />
 @endsection
 @section('content')
 <div class="container">
@@ -12,7 +13,6 @@
                     Add Student
                 </div>
                 <div class="card-body">
-                    
                     @if(Session::has('student_added'))
                     <div class="alert alert-success" role="alert">
                         {{Session::get('student_added')}}
@@ -47,7 +47,8 @@
 
 @endsection
 @section('add-cdn')
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous"></script>
 <script>
  function previewFile(input){
      var file= $("input[type=file]").get(0).files[0];
@@ -60,4 +61,20 @@
      }
  }
 </script>
+
+@if(Session::has('student_added'))
+<script>
+    toastr.success("{!! Session::get('student_added') !!}")
+</script>
+@endif
+@if(Session::has('student_added'))
+<script>
+    swal("Congrats","{!! Session::get('student_added') !!}","success",{
+        button:"OK",
+    })
+</script>
+@endif
+
+
+
 @endsection
